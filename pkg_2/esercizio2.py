80afcc288e48d060610516892d0b56ae7130d171
Gerry <gerrycorbisiero@gmail.com>
2018-12-17 13:06:21 +0100

    es2

--- pkg_2/esercizio2.py
+++ pkg_2/esercizio2.py
@@ -3,6 +3,7 @@ from Flight import *
 from typing import List
 from datetime import datetime
 from TdP_collections.priority_queue.adaptable_heap_priority_queue import AdaptableHeapPriorityQueue
+from TdP_collections.list.positional_list import PositionalList
 from utils import read_from_file
 
 def backtrack(arrival_time,departure_time,min_waiting,cost,total):
@@ -14,7 +15,7 @@ def backtrack(arrival_time,departure_time,min_waiting,cost,total):
 
 def find_route(flights :List[Flight], start :Airport,b :Airport,t):
 
-    priority_queue = AdaptableHeapPriorityQueue();
+    route = PositionalList()
 
     # Construzione mappa di adiacenza, dato un aereoporto (chiave) ottengo la lista di tutti i voli che partono da questo)
     airports_flights = dict()
@@ -25,60 +26,116 @@ def find_route(flights :List[Flight], start :Airport,b :Airport,t):
             else:
                 airports_flights[s(flight)] = [flight]
 
-    return Dijkstra(airports_flights, start, b, t)
+    min_tree_path = Dijkstra(airports_flights, start, b, t)
+
+    flight_tmp = min_tree_path[b]
+    print(flight_tmp)
+    route.add_first(flight_tmp)
+    flight_tmp = min_tree_path[s(flight_tmp)]
+
+    while s(flight_tmp) != start:
+        route.add_first(flight_tmp)
+        flight_tmp = min_tree_path[s(flight_tmp)]
+
+    for e in route:
+        print(e)
+    return route
 
 
 
 
 def Dijkstra(airports_flights , start :Airport,b :Airport,t):
 
-    d = {}                                 # d[v] è un limite superiore alla distanza tra aereoporto start e un altro aereoporto
-    cloud = {}                             # dizionario che mappa aereoporto in d[aereoporto], dove questa eticetta rappresenta il costo per andare da start a aereoporto nel minor tempo
-    pq = AdaptableHeapPriorityQueue()      #vertice v avrà key d[v]
+    D = {}                                 # D[v] è un limite superiore alla distanza tra aereoporto start e un altro aereoporto
+    cloud = {}                             # dizionario che mappa aereoporto in D[aereoporto], dove questa eticetta rappresenta il costo per andare da start a aereoporto nel minor tempo
+    pq = AdaptableHeapPriorityQueue()      #vertice v avrà key D[v]
     pqlocator = {}                         #dizionario che mappa un vertice nel suo pq locator per trovarlo in O(1)
-    min_tree_path = []
+    min_tree_path = {}
 
     airports = airports_flights.keys()
-    flights : List[Flight] = airports_flights.values()
+    flights : List[Flight] = []
+    for list_flight in airports_flights.values():
+        flights += list_flight
 
     for airport in airports:
         if airport is start:
-            d[airport] = 0
+            D[airport] = 0
         else:
-            d[airport] = float('inf')
-        pqlocator[airport] = pq.add(d[airport], airport)  #salva il locator per i fututi aggiornamenti
+            D[airport] = float('inf')
+        pqlocator[airport] = pq.add(D[airport], airport)  #salva il locator per i fututi aggiornamenti
 
     while not pq.is_empty() or not b in cloud:       # perchè se b è in cloud ho già trovato il percorso minimo che mi porta da a a b
         key, u = pq.remove_min()
         cloud[u] = key   #inserisce u nella soluzione
         del pqlocator[u]
-        for flight in flights[u]:    #per tutti gli archi uscenti da u, quindi per tutti i voli che partono da quell'aereoporto
+        for flight in airports_flights[u]:    #per tutti gli archi uscenti da u, quindi per tutti i voli che partono da quell'aereoporto
             airport = d(flight)   #prendo l'aereoporto di arrivo
             if airport not in cloud:
-                costo = c(d(airport))+ (l(flight) - a(flight))  # dove per costo si intende costo viaggio + costo attesa aereoporto d'arrivo
-                if d[u] + costo < d[airport]:
-                    d[airport] = d[u] + costo
-                    pq.update(pqlocator[airport], d[airport], airport)  #aggiorna d[v] nella pq
+                costo = c(airport)+ (a(flight) - l(flight))  # dove per costo si intende costo viaggio + costo attesa aereoporto D'arrivo
+                if D[u] + costo < D[airport]:
+                    D[airport] = D[u] + costo
+                    pq.update(pqlocator[airport], D[airport], airport)  #aggiorna D[v] nella pq
 
     if b not in cloud:
         return None
     # arrivato qui avrò un dizionario con chiavi i vertici messi nella soluzione e valori i costi per arrivarci da s
 
-    # for airport in cloud:
-    #     if airport is not start:
-    #         for flight in flights:
-    #             if d(flight) == airport:   #conssidera gli archi enranti, i voli che entrano in questo aereoporto
-    #                 u = s(flight)          #se flight è uno di questi, mettilo in u
-    #                 costo = c(flight) + (a(flight) - l(flight))
-    #                 if d[airport] == d[u] + costo:
-    #                     min_tree_path[airport] = flight
+    for airport in cloud:
+        if airport is not start:
+            for flight in flights:
+                if d(flight) == airport:   #conssidera gli archi enranti, i voli che entrano in questo aereoporto
+                    u = s(flight)          #se flight è uno di questi, mettilo in u
+                    costo = c(airport) + (a(flight) - l(flight))
+                    if D[airport] == D[u] + costo:
+                        min_tree_path[airport] = flight
 
-    u = b
+    if min_tree_path is None:
+        print("ho sbagliato qualcosa")
+
+    return min_tree_path
+
+
+    # u = b
+    #
+    # for flight in flights:
+    #     if d(flight) == u:
+    #         costo = c(u) + (a(flight) - l(flight))
+    #         d_u = cloud[u]
+    #         if cloud[airport] == cloud[u] + costo:
+    #             min_tree_path.append(flight)
+    #
+    # return min_tree_path.reverse()
+
+
+if __name__ == "__main__":
+
+    airports,flights=read_from_file("test1.txt")
+
+    """for airport in airports:
+        print(airport)
+
     for flight in flights:
-        if d(flight) == u:
-            costo = c(u) + (a(flight) - l(flight))
-            d_u = cloud[u]
-            if cloud[airport] == cloud[u] + costo:
-                min_tree_path.append(u)
+        print(flight)"""
 
-    return min_tree_path.reverse()
+    start = airports[0]
+    end = airports[3]
+    starting_time = datetime.strptime("12:00", "%H:%M").time()
+    total_time = datetime.strptime("23:00", "%H:%M").time()
+
+    start_time_minutes = starting_time.hour*60 + starting_time.minute
+    total_time_minutes = total_time.hour*60 + total_time.minute
+
+    path = find_route(flights,start,end,start_time_minutes)
+
+
+
+    print("\n\nPercorso da "+str(start)+" a "+str(end)+" in "+str(total_time_minutes)+" minuti ")
+    print("Partenza alle "+str(starting_time))
+
+    print("--------------PATH----------- ")
+
+    if(path is not None):
+        for flight in path:
+         print(flight)
+    else:
+        print("Nessun percorso trovato")
